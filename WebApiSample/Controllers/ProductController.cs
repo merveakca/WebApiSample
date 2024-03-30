9da5e2ce66c5f4e5ef4786c34b4f5857dbf2646c
merveakca <smkirac@gmail.com>
2024-03-30 11:57:47 +0300

    Add project files.

---
+++ WebApiSample/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    [HttpGet]
+    public List<Product> GetAll()
+    {
+        NorthwndContext db = new NorthwndContext();
+        List<Product> products = db.Products.ToList();
+        return products;
+    }
+
+    [HttpGet("{id}")]
+    public Product Get(int id)
+    {
+        NorthwndContext db = new NorthwndContext();
+        Product product = db.Products.FirstOrDefault(x => x.ProductId == id);
+        return product;
+    }
+}
---
+++ WebApiSample/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiSample.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
