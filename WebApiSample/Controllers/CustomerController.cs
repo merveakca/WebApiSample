ba6243aca845e0ddfe78ca760f412ced4809c10f
merveakca <smkirac@gmail.com>
2024-04-15 22:49:36 +0300

    .

---
+++ WebApiSample/Controllers/CustomerController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        NorthwndContext db = new NorthwndContext();
+        List<Customer> customers = db.Customers.ToList();
+        return Ok(customers);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        NorthwndContext db = new NorthwndContext();
+
+        Customer customer = db.Customers.FirstOrDefault(q => q.CustomerId == id);
+
+        if (customer == null)
+        {
+            return NotFound();
+        }
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Customer customer)
+    {
+        NorthwndContext db = new NorthwndContext();
+        db.Customers.Add(customer);
+        db.SaveChanges();
+        return Ok(db);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        NorthwndContext db = new NorthwndContext();
+        Customer customer = db.Customers.FirstOrDefault(q => q.CustomerId == id);
+
+        if (customer == null)
+        {
+            return NotFound("Böyle bir customer bulunamadı");
+        }
+
+        db.Customers.Remove(customer);
+        db.SaveChanges();
+        return Ok("Silme işlem başarılı");
+    }
+
+    [HttpPut]
+    public IActionResult Put(Customer customer)
+    {
+        NorthwndContext db = new NorthwndContext();
+
+        Customer entity = db.Customers.FirstOrDefault(q => q.CustomerId == customer.CustomerId);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        entity.CompanyName = customer.CompanyName;
+        entity.ContactName = customer.ContactName;
+        entity.Phone = customer.Phone;
+
+        db.SaveChanges();
+        return Ok(entity);
+    }
+}
--- WebApiSample/Controllers/SupplierController.cs
+++ WebApiSample/Controllers/SupplierController.cs
@@ -18,8 +18,8 @@ public class SupplierController : ControllerBase
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-        NorthwndContext db=new NorthwndContext();
-        Supplier supplier = db.Suppliers.FirstOrDefault(x=>x.SupplierId==id);
+        NorthwndContext db = new NorthwndContext();
+        Supplier supplier = db.Suppliers.FirstOrDefault(x => x.SupplierId == id);
 
         if (supplier == null)
         {
@@ -27,5 +27,51 @@ public class SupplierController : ControllerBase
         }
 
         return Ok(supplier);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Supplier supplier)
+    {
+        NorthwndContext db = new NorthwndContext();
+        db.Suppliers.Add(supplier);
+        db.SaveChanges();
+        return Ok(db);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        NorthwndContext db = new NorthwndContext();
+        Supplier supplier = db.Suppliers.FirstOrDefault(x => x.SupplierId == id);
+
+        if(supplier == null)
+        {
+            return NotFound("Böyle bir supplier bulunamadı");
+        }
+
+        db.Suppliers.Remove(supplier);
+        db.SaveChanges();
+        return Ok("silme işlemi başarılı");
+    }
+
+    [HttpPut]
+    public IActionResult Put(Supplier supplier)
+    {
+        NorthwndContext db = new NorthwndContext();
+
+        Supplier entity = db.Suppliers.FirstOrDefault(x=>x.SupplierId == supplier.SupplierId);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        entity.SupplierId = supplier.SupplierId;
+        entity.CompanyName = supplier.CompanyName;
+        entity.ContactName = supplier.ContactName;
+        entity.ContactTitle = supplier.ContactTitle;
+
+        db.SaveChanges();
+        return Ok(entity);
     }
 }
